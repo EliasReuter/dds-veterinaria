0faa14952196f7ca5fce827dbb6da00a3c7d1371
EliasReuter <EliasReuter>
2020-09-06 17:34:13 -0300

    Revision orm

--- backend/src/main/java/db/EntityManagerHelper.java
+++ backend/src/main/java/db/EntityManagerHelper.java
@@ -1,5 +1,4 @@
 package db;
-
 import java.util.function.*;
 
 import javax.persistence.EntityManager;
@@ -28,13 +27,11 @@ public class EntityManagerHelper {
     }
 
     public static EntityManager getEntityManager() {
-       /* EntityManager manager = threadLocal.get();
+        EntityManager manager = threadLocal.get();
         if (manager == null || !manager.isOpen()) {
             manager = emf.createEntityManager();
             threadLocal.set(manager);
-        }*/
-        EntityManager manager = emf.createEntityManager();
-        threadLocal.set(manager);
+        }
         return manager;
     }
 
