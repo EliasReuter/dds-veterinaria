aec095ff3afd464b3f4a81a254a142df2fc80788
EliasReuter <EliasReuter>
2020-09-12 15:41:11 -0300

    Arreglo problema Entity Manager

--- backend/src/main/java/domain/repositories/daos/DAOHibernate.java
+++ backend/src/main/java/domain/repositories/daos/DAOHibernate.java
@@ -3,6 +3,7 @@ package domain.repositories.daos;
 import db.EntityManagerHelper;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
@@ -16,16 +17,20 @@ public class DAOHibernate<T> implements DAO<T> {
 
     @Override
     public List<T> buscarTodos() {
+
         CriteriaBuilder builder = EntityManagerHelper.getEntityManager().getCriteriaBuilder();
         CriteriaQuery<T> critera = builder.createQuery(this.type);
         critera.from(type);
         List<T> entities = EntityManagerHelper.getEntityManager().createQuery(critera).getResultList();
+        EntityManagerHelper.closeEntityManager();
         return entities;
     }
 
     @Override
     public T buscar(int id) {
-        return EntityManagerHelper.getEntityManager().find(type, id);
+        T obj = EntityManagerHelper.getEntityManager().find(type, id);
+        EntityManagerHelper.closeEntityManager();
+        return obj;
     }
 
     @Override
@@ -33,6 +38,7 @@ public class DAOHibernate<T> implements DAO<T> {
         EntityManagerHelper.getEntityManager().getTransaction().begin();
         EntityManagerHelper.getEntityManager().persist(unObjeto);
         EntityManagerHelper.getEntityManager().getTransaction().commit();
+        EntityManagerHelper.closeEntityManager();
     }
 
     @Override
@@ -40,6 +46,7 @@ public class DAOHibernate<T> implements DAO<T> {
         EntityManagerHelper.getEntityManager().getTransaction().begin();
         EntityManagerHelper.getEntityManager().merge(unObjeto);
         EntityManagerHelper.getEntityManager().getTransaction().commit();
+        EntityManagerHelper.closeEntityManager();
     }
 
     @Override
@@ -47,5 +54,6 @@ public class DAOHibernate<T> implements DAO<T> {
         EntityManagerHelper.getEntityManager().getTransaction().begin();
         EntityManagerHelper.getEntityManager().remove(unObjeto);
         EntityManagerHelper.getEntityManager().getTransaction().commit();
+        EntityManagerHelper.closeEntityManager();
     }
 }
